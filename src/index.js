505e23cd16ee1eb898ea2f2e6e0ed9f7451c4051
Gleb Stashinsky <tragy@list.ru>
2021-03-09 00:04:46 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (array && array.length) ? array.sort((a, b) => a-b).shift() : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array && array.length) ? array.sort((a, b) => a-b).pop() : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (array && array.length) ? array.reduce((avg, val) => avg + val) / array.length : 0;
 }
